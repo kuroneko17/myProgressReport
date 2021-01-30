51d27ae3763c6dd53e050f433b7ce511786a1d26
testSYLou <testSYLou@gmail.com>
2021-01-30 16:46:23 +0800

    first commit by testSYLou

---
+++ download.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+int = 1
+while (( $int < 17)) 
+do
+    https://labfile.oss.aliyuncs.com/courses/1330/python$int.png
+    echo $int
+    let $int++
+done
+
+wget https://labfile.oss.aliyuncs.com/courses/1330/linux.png
+wget https://labfile.oss.aliyuncs.com/courses/1330/git.png
