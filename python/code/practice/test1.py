53636e61283cd3c99cdc769f78d395294a6a69b1
kuro <kuroneko961718@gmail.com>
2021-02-07 01:37:09 +0800

    learn note

---
+++ python/code/practice/test1.py
@@ -0,0 +1,21 @@
+maxDay = int(input('请输入最大天数：'))
+for i in range(1,maxDay+1):
+    print('书恒走的第{}天，想他'.format(i))
+
+# DEF Test :
+#    __INIT__():
+
+
+# 导入模块同名类 不执行
+# 模块名同名类
+# import sys
+# sys.path.append('C:\Users\MakeNoSense\Desktop\python_test')
+# from test import *
+
+# python解释器导入.py文件并执行
+# 斜杠/，转义字符转义文件路径(C:\Users\MakeNoSense\Desktop)，文件路径前加r、
+# https://www.cnblogs.com/rychh/p/9743864.html
+# 绝对路径，
+# import sys
+# sys.path.append(r'C:\Users\MakeNoSense\Desktop')
+# from HelloWorld import *
